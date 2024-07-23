76b1c2ac2645fd7158940d0abab096701c2ec3a2
Nikhil Swaminathan <2429410+swaminator@users.noreply.github.com>
2024-07-23 15:45:33 -0700

    Update actions.cy.js

--- cypress/e2e/2-advanced-examples/actions.cy.js
+++ cypress/e2e/2-advanced-examples/actions.cy.js
@@ -9,7 +9,7 @@ context('Actions', () => {
 
   it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
-    cy.get('.action-email').type('fake@email.com')
+    cy.get('.action-email').type('fake2@email.com')
     cy.get('.action-email').should('have.value', 'fake@email.com')
 
     // .type() with special character sequences
